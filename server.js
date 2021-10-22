d086b2ca51098eb7b3d818f3de43dde0cebb3791
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-10-21 18:05:24 -0600

    added dependencies

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 // import sequelize connection
+const sequelize = require("sequelize");
+// import mysql2
+const mysql = require('mysql2');
+// import dotenv
+require('dotenv').config();
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: process.env.NAME,
+  database: process.env.HOST
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +24,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
